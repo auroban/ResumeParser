61ca2c74b2cd6b3c6711222249e242c5dfdc1c24
Arijit Banerjee <auro.ban.ari@gmail.com>
2018-02-23 01:43:51 +0530

    Tested PDF parsing

--- src/TestParser.java
+++ src/TestParser.java
@@ -1,7 +1,13 @@
+import java.io.IOException;
 
 public class TestParser {
 
 	public static void main(String[] args) {
+		try {
+			System.out.println(new ResumeParser().parseResumeInJSON(""));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
-
 }
